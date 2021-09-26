f703cbb691428930b88da83b0bbccbdbb40ff028
Lê Minh Thủy <40124962+thuypq123@users.noreply.github.com>
2021-09-26 09:04:14 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const url = "mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_
 
 async function connect(){
     try {
-        await mongoose.connect(url,{ useUnifiedTopology: true });
+        await mongoose.connect("mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_io?retryWrites=true&w=majority",{ useUnifiedTopology: true });
         console.log('Success Connected');
     } catch (error) {
         console.log(error);
