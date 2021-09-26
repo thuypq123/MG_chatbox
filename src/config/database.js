723802e85d983e366771e2b08f91b1f82f0e1dd5
Lê Minh Thủy <40124962+thuypq123@users.noreply.github.com>
2021-09-26 09:21:03 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,16 @@ const url1 = "mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket
 const url = "mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_io?retryWrites=true&w=majority"
 
 //================================
-
+const options = {
+  autoIndex: false, // Don't build indexes
+  poolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
 async function connect(){
     try {
-        await mongoose.connect("mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_io?retryWrites=true&w=majority",{ useUnifiedTopology: true });
+        await mongoose.connect("mongodb+srv://socket_io:thuy1234@cluster0.uhkg4.mongodb.net/socket_io?retryWrites=true&w=majority",options);
         console.log('Success Connected');
     } catch (error) {
         console.log(error);
